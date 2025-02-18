34be8a1910190f89ec694ec1b7752ef3a0bbfb51
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-02-18 09:16:45 -0300

    Add a very basic structure for the argument parsing

--- src/main.c
+++ src/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <argp.h>
+#include <getopt.h>
 
-int main(void)
+const char *argp_program_version      = "Smart File Organizer v0.1";
+const char *argp_program_error_report = "Open a issue in https://github.com/Shadabdullah/SmartFileOrganizer";
+
+struct argp_option argument_options[] = {
+  /*
+      Setup options to be used on the program
+  */
+
+  {0} // Necessery at the end of the argument list
+};
+
+int main(int argc, const char **argv)
 {
   printf("Smart File Manager CLI");
   return 0;
