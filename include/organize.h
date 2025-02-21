8f97fba474f0e46cf8d31bfc2f239825d4c777fb
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-02-21 14:52:32 -0300

    Create a header file for the organize module

--- include/organize.h
+++ include/organize.h
@@ -2,7 +2,10 @@
 #define SDO_ORGANIZE_H
 #include <string.h>
 #include <stdint.h>
-
+#include <dirent.h>
 #include "file_type_detector.h"
+#include "errors.h"
+
+int8_t organize(const char *path);
 
 #endif
