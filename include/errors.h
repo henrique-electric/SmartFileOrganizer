2272e8bbde5ba43a973fee9803b825a3a64c7996
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-02-21 14:51:33 -0300

    Move the return codes from cli and organize modules

--- include/errors.h
+++ include/errors.h
@@ -2,3 +2,11 @@
 #define FAILED_INIT_MAGIC 4042
 #define FAILED_LOAD_MAGIC_DATABASE 4043
 #define FAILED_GET_MIME_TYPE 4044
+
+// Defines from organize module
+#define ERROR_ON_ORGANIZE_DIR   -1
+#define SUCCESS_ON_ORGANIZE_DIR  0
+
+// Defines from ocli module
+#define  USER_COMMAND_READ_ERROR  -1
+#define  USER_COMMAND_READ_SUCCESS 0
