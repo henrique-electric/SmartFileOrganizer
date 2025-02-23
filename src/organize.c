1747082db5b584ff6a0cbc993d7a018a6100525e
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-02-23 13:51:53 -0300

    Remove useless function, since the type buffer was split in type and format

--- src/organize.c
+++ src/organize.c
@@ -1,23 +1,10 @@
 #include "../include/organize.h"
 
-
-/*
-    A simple functions to check if the file is a directory
-*/
-static bool check_is_a_dir(const struct file *file) {
-    if (file == NULL)
-        return false;
-
-    char *short_type = strtok(file->mime_type, '/'); // Get the string before the '/', which is the type
-    if (strcmp(short_type, "inode") == 1) // check if it is an inode (directory)
-        return false; // it is a directory, not a valid type
-
-    return true; // the file has a valid type
-}
-
-static int8_t handle_types(const struct dirent *file, const file_info *file_info) {
-    if (file == NULL || file_info == NULL)
+static int8_t handle_types(const file_info *file_info) {
+    if (file_info == NULL)
         return ERROR_ON_HANDLE_TYPE; 
+
+    
     
 }
 
@@ -37,7 +24,7 @@ int8_t organize(const char *path) {
         return ERROR_ON_ORGANIZE_DIR;
 
     while ((file_on_dir = readdir(&dir_pointer)) != NULL) {
-        if (check_is_a_dir(path) == true) {
+        if (strcmp(file_type.file_type, "inode") == 0) {
             continue; // the file is a directory, jump to the next file
         } else {
             get_file_type(file_on_dir->d_name, &file_type);
