6ec08ccbde9f3da3a331729002b4c3845208f218
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-03-27 21:26:38 -0300

    Implemented the terminal function

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 
 #include "../include/file_type_detector.h"
 #include "../include/organize.h"
+#include "../include/cli.h"
 
 file_info output;
 char program_path[256]; // where the user called the program
@@ -9,39 +10,18 @@ char *file_path = NULL; // this variable is used to concat where the program was
 
 int main(int argc, char *argv[])
 {
-  printf("Smart File Manager CLI");
 
   if (argc > 1)
   {
     char *home = getenv("HOME");
     getcwd(program_path, sizeof(program_path));
-    file_path = strcat(program_path, "/");
-    file_path = strcat(home, "/");
-    file_path = strcat(file_path, argv[1]);
-    file_path = strcat(file_path, "/");
+    strcat(program_path, "/");
+    strcat(home, "/");
+    strcat(program_path, argv[1]);
+    strcat(program_path, "/");
     
-
-    /*
-    int result = get_file_type(file_path, &output);
-    get_file_size(file_path, &output);
-    get_file_extension(file_path, &output);
-    
-    if (result == 0)
-    {
-      printf("File Description \n");
-      printf("File type: %s\n", output.type);
-      printf("File details: %s\n", output.details);
-      printf("File size: %ld bytes\n", output.file_size);
-      printf("File extension: %s\n", output.extension);
-    }
-    //Getting more info about the files and categorizing them ( to do )
-    type_categories(file_path, &output);
-
-    */
-    //Test organizing function
-    int organize_output = organize(file_path);
-    //printf("organize returned: %d\n", organize_output);
-
+  } else {
+    terminal();
   }
 
   return 0;
