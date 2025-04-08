6995e2ba23db64e3dc02dba604926d286d6ee58d
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-04-07 23:42:37 -0300

    Add a define for the log file name, and add members to the struct

--- include/log.h
+++ include/log.h
@@ -1,16 +1,23 @@
 #ifndef SFO_LOG_H
+#define SFO_LOG_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include "utils.h"
 
-struct sfo_log_struct {
+
+#define SFO_LOG_FILE_NAME "log.txt" // name for the log file (can be changed as needed)
+
+typedef struct sfo_log_struct {
     time_t raw_log_time;
     struct tm *time_info;
+    char *ascii_time;
     char *msg_buff;
-};
-typedef struct sfo_log_struct sfo_log;
+    char *file_path;
+    FILE *log_file_ptr;
+} sfo_log;
 
-void write_log(sfo_log *log);
+void write_log(const char *msg);
 
 #endif
