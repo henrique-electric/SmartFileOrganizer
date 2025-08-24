5efc9789ead9e4e53988dbb72dd129efec83bbbb
Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-08-24 00:20:26 +0000

    Change the syntax

--- src/main.c
+++ src/main.c
@@ -15,7 +15,8 @@ char *file_path = NULL; // this variable is used to concat where the program was
 struct argp_option arguments[] = {
     {"organize", 'o', "organize_path", 0, "Organize {path}", 0},
     {"backup", 'b', "backup_path", 0, "Backup {path}", 0},
-    {NULL}};
+    {NULL}
+};
 
 struct argument_state {
   bool use_organize;
