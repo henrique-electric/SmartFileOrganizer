53ddbceabb3ee686139c6d96e2c99fc7ad38ea6f
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2025-08-27 23:53:13 +0000

    Add colors available to print on terminal

--- include/utils.h
+++ include/utils.h
@@ -4,8 +4,18 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 
 #define SFO_HOME_LEN 512
+
+// Colors of output
+#define RED_COLOR       "\033[0;31m"
+#define GREEN_COLOR     "\033[0;32m"
+#define YELLOW_COLOR    "\033[0;33m"
+#define BLUE_COLOR      "\033[0;34m"
+#define MAGENTA_COLOR   "\033[0;35m"
+#define CYAN_COLOR      "\033[0;36m"
+#define WHITE_COLOR     "\033[0;37m"
 
 char *get_sfo_home(void);
 void flush_std();
